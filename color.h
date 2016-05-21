b04d4064711d3abfefa4dd39239d3cdcb0b37c6e
shaoguangleo <shaoguangleo@gmai.com>
2016-05-21 21:49:34 +0800

    Add useful color in terminal applications

---
+++ color.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name : color.h
+	> Author    : Guo Shaoguang 
+	> Mail      : shaoguangleo@gmail.com
+    > Function  : color text in terminal application in linux OS
+ ************************************************************************/
+
+#ifndef _COLOR_H
+#define _COLOR_H
+#define COLOR_NORMAL    "\x1B[0m"
+#define COLOR_RED       "\x1B[31m"
+#define COLOR_GREEN     "\x1B[32m"
+#define COLOR_YELLOW    "\x1B[33m"
+#define COLOR_BLUE      "\x1B[34m"
+#define COLOR_MAGENTA   "\x1B[35m"
+#define COLOR_CYAN      "\x1B[36m"
+#define COLOR_WHITE     "\x1B[37m:w
+#endif
